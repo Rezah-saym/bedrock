3cc5d1ae4e21a538845a434e7ee418f62c044186
rezah <hlrr@esokia-webagency.com>
2023-05-08 14:25:22 +0300

    8_may_2023

---
+++ bedrock/web/app/themes/react_theme/tpl-page-home.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Template Name: Page home
+ * Template for Home page
+ * 
+ * @package bootstrap-basic
+ */
+
+get_header(); 
+?>
+      <div id="root"></div>
+
+<?php
+get_footer();
+?>
+
+
+
+
+
+
+
