b1fb8feb39f44736e26700356005fff2e072a38d
rezah <hlrr@esokia-webagency.com>
2023-05-11 14:51:07 +0300

    11_mai_2023

---
+++ Travau 11 mai 2023/bedrock/web/app/themes/react_theme/tpl-page-login.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Template Name: Page log in
+ * Template for log in
+ * 
+ * @package bootstrap-basic
+ */
+
+get_header(); 
+?>
+    <div class="container-fluid">
+        <div id="login_page" class="row"></div>
+    </div>
+
+<?php
+get_footer();
+?>
+
