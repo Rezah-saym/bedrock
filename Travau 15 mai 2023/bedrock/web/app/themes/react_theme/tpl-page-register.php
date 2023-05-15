360fa557d97119d6c0edb4ab866de9bbf2e8ffa7
rezah <hlrr@esokia-webagency.com>
2023-05-15 16:07:26 +0300

    15_mai_2023

---
+++ Travau 15 mai 2023/bedrock/web/app/themes/react_theme/tpl-page-register.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Template Name: Page Register
+ * Template for Register page
+ * 
+ * @package bootstrap-basic
+ */
+
+get_header(); 
+?>
+     <div class="container-fluid page_top">
+            <div id="register_page" class="row"></div>
+     </div>
+<?php
+get_footer();
+?>
