7d827cde8dae2ff44a17f0c60a5155030c08eaaf
rezah <hlrr@esokia-webagency.com>
2023-05-17 18:14:11 +0300

    pagination

---
+++ Travau 17 mai 2023/bedrock/web/app/themes/react_theme/tpl-page-author.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Template Name: Page Author
+ * Template for author page
+ * 
+ * @package bootstrap-basic
+ */
+
+get_header(); 
+?>
+    <div class="container-fluid page_top">
+        <div id="author_page"></div>
+    </div>
+
+<?php
+get_footer();
+?>
+
